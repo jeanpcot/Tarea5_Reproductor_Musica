25d62cadb95dd236738ba25ce4f04dd9e4706fba
charlie-rzu <carla.r.acevedo@hotmail.com>
2024-05-28 22:18:38 -0500

    Update: Eliminacion de codigo duplicado

--- src/logica/Menu.java
+++ src/logica/Menu.java
@@ -1,5 +1,6 @@
 import logica.Album;
 import logica.Cancion;
+import logica.Usuario;
 
 import java.util.ArrayList;
 import java.util.InputMismatchException;
@@ -38,25 +39,24 @@ public class Menu {
                         continuar = false;
                         break;
                     case 1:
-                        Album albumCreado = crearAlbum();
+                        Album albumCreado = Usuario.crearAlbum(scanner);
                         albumes.add(albumCreado);
                         System.out.println("Álbum creado exitosamente!");
                         break;
                     case 2:
-                        mostrarAlbumes();
+                        Album.mostrarAlbumes(albumes);
                         System.out.println("Elija el álbum en que va a agregar la música (ingrese el número del álbum):");
                         int numeroAlbum = scanner.nextInt();
                         scanner.nextLine();
                         if (numeroAlbum > 0 && numeroAlbum <= albumes.size()) {
-                            Cancion cancionCreada = crearCancion();
-                            albumes.get(numeroAlbum - 1).agregarCancion(cancionCreada);
-                            System.out.println("Canción agregada exitosamente!");
+                            Album albumSeleccionado = albumes.get(numeroAlbum - 1);
+                            albumSeleccionado.registrarCancion();
                         } else {
                             System.out.println("Número de álbum inválido.");
                         }
                         break;
                     case 3:
-                        mostrarAlbumes();
+                        Album.mostrarAlbumes(albumes);
                         System.out.println("Elija el álbum que desea ver sus músicas (ingrese el número del álbum):");
                         int numeroAlbumConsulta = scanner.nextInt();
                         scanner.nextLine();
@@ -71,18 +71,7 @@ public class Menu {
                     case 4:
                         System.out.println("Por favor, introduce el año del álbum:");
                         String año = scanner.nextLine();
-                        Album albumAux = null;
-                        for (Album album : albumes) {
-                            if (año.equals(album.getAño())) {
-                                albumAux = album;
-                                break;
-                            }
-                        }
-                        if (albumAux == null) {
-                            System.out.println("El álbum no existe");
-                        } else {
-                            System.out.println(albumAux);
-                        }
+                        Usuario.listarAlbumsPorAnio(año);
                         break;
                     default:
                         System.out.println("Opción no válida. Por favor, ingrese un número válido del menú.");
@@ -92,32 +81,5 @@ public class Menu {
                 scanner.nextLine();
             }
         }
-    }
-
-    private Cancion crearCancion() {
-        System.out.println("Por favor, introduce el título de la canción:");
-        String titulo = scanner.nextLine();
-        System.out.println("Por favor, introduce la duración de la canción en minutos y segundos (separados por un espacio):");
-        String[] duracionStr = scanner.nextLine().split(" ");
-        int[] duracion = {Integer.parseInt(duracionStr[0]), Integer.parseInt(duracionStr[1])};
-        return new Cancion(titulo, duracion);
-    }
-
-    private void mostrarAlbumes() {
-        for (int i = 0; i < albumes.size(); i++) {
-            System.out.println((i + 1) + ". " + albumes.get(i).toString());
-        }
-    }
-
-    private Album crearAlbum() {
-        System.out.println("Por favor, introduce el nombre del álbum:");
-        String nombre = scanner.nextLine();
-        System.out.println("Por favor, introduce los artistas del álbum (separados por comas):");
-        String artistas = scanner.nextLine();
-        System.out.println("Por favor, introduce el año de lanzamiento del álbum:");
-        String añoLanzamiento = scanner.nextLine();
-        System.out.println("Por favor, introduce la disquera del álbum:");
-        String disquera = scanner.nextLine();
-        return new Album(nombre, artistas, añoLanzamiento, disquera);
     }
 }
