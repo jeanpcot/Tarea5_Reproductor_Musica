0d13d798433036a124c14b678442d317eff220bd
charlie-rzu <carla.r.acevedo@hotmail.com>
2024-05-28 22:18:28 -0500

    Update: Eliminacion de codigo duplicado

--- src/logica/Usuario.java
+++ src/logica/Usuario.java
@@ -5,40 +5,38 @@ import java.util.Scanner;
 
 public class Usuario {
     private String nombre;
-    private ArrayList<Album> albums;
+    private static ArrayList<Album> albums;
     public Usuario(String nombre) {
         this.nombre = nombre;
         this.albums = new ArrayList<>();
     }
+
     public void agregarAlbum(Album album) {
         this.albums.add(album);
     }
-    public Album llenarAlbum(){
-        Scanner scanner = new Scanner(System.in);
-        Album album = new Album();
-        System.out.println("Ingresa el nombre: ");
-        album.setNombre(scanner.nextLine());
-        System.out.println("Ingresa el artista/as: ");
-        album.setArtistas(scanner.nextLine());
-        System.out.println("Ingresa el año de lanzamiento: ");
-        album.setAnioLanzamiento(scanner.nextLine());
-        System.out.println("Ingresa la disquera: ");
-        album.setDisquera(scanner.nextLine());
-        scanner.close();
-        return album;
-    }
-    public void registrarAlbum()
-    {
-        Album album = llenarAlbum();
-        agregarAlbum(album);
 
+    public static Album crearAlbum(Scanner scanner) {
+        System.out.println("Por favor, introduce el nombre del álbum:");
+        String nombre = scanner.nextLine();
+        System.out.println("Por favor, introduce los artistas del álbum (separados por comas):");
+        String artistas = scanner.nextLine();
+        System.out.println("Por favor, introduce el año de lanzamiento del álbum:");
+        String añoLanzamiento = scanner.nextLine();
+        System.out.println("Por favor, introduce la disquera del álbum:");
+        String disquera = scanner.nextLine();
+        return new Album(nombre, artistas, añoLanzamiento, disquera);
     }
 
-    public void listarAlbumsPorAnio(String anioSolicitado){
+    public static void listarAlbumsPorAnio(String anioSolicitado){
+        boolean encontrado = false;
         for (Album album : albums) {
-            if(album.getAño().equalsIgnoreCase(anioSolicitado)){
+            if (album.getAño().equalsIgnoreCase(anioSolicitado)) {
                 System.out.println(album);
+                encontrado = true;
             }
+        }
+        if (!encontrado) {
+            System.out.println("No se encontraron álbumes para el año " + anioSolicitado);
         }
     }
 }
